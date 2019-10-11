228d19dfb3a1a95fe7a45935e42ddd6635f64d8d
Kyle Richardson <kyle.richardson1@gmail.com>
2019-10-10 23:09:00 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import {Route} from "react-router-dom"
 import TOS from "./TOS"
-// import Users from "./Users"
 
 import FormikLoginForm from "./LoginForm.js"
 
 function App() {
-  // const [userList, setUserList] = useState({
-  //   name: 'kyle richardson',
-  //   email: 'kyle.r@me.com',
-  //   password: '123456',
-  //   account: 'gold'
-  // })
 
   return (
     <div className="App">
       <Route path="/tos" component={TOS} />
       <Route exact path="/" component={FormikLoginForm} />
-      {/* <Users userList={userList}/> */}
     </div>
   );
 }
